4ff054ca217131eb2759fedb5f02a63e3ea17e6e
Saad Aftab <saadaftab1990@gmail.com>
2018-05-08 13:23:40 +0500

    Minor changes

--- app/src/main/java/com/wow/app/random/utilities/Constants.java
+++ app/src/main/java/com/wow/app/random/utilities/Constants.java
@@ -2,7 +2,7 @@ package com.wow.app.random.utilities;
 
 public class Constants {
 
-    public static int PRODUCER_SECONDS = 3000;
-    public static int CONSUMER_SECONDS = 4000;
+    public static final int PRODUCER_SECONDS = 3000;
+    public static final int CONSUMER_SECONDS = 4000;
 
 }
